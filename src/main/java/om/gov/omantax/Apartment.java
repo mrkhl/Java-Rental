c7dc9ed8d52a8a2e920aed9057d26baf17daeb76
mrkhl <48253036+mrkhl@users.noreply.github.com>
2023-07-17 09:30:09 +0400

    rental Form Genaratore.. this project will create a rental form

--- src/main/java/om/gov/omantax/Apartment.java
+++ src/main/java/om/gov/omantax/Apartment.java
@@ -1,4 +1,36 @@
 package om.gov.omantax;
 
 public class Apartment {
+    private int apartmentID;
+    private double apartmentPrice;
+
+
+    public Apartment(int apartmentID, double apartmentPrice) {
+        this.apartmentID = apartmentID;
+        this.apartmentPrice = apartmentPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Apartment{" +
+                "apartmentID=" + apartmentID +
+                ", apartmentPrice=" + apartmentPrice +
+                '}';
+    }
+
+    public int getApartmentID() {
+        return apartmentID;
+    }
+
+    public void setApartmentID(int apartmentID) {
+        this.apartmentID = apartmentID;
+    }
+
+    public double getApartmentPrice() {
+        return apartmentPrice;
+    }
+
+    public void setApartmentPrice(double apartmentPrice) {
+        this.apartmentPrice = apartmentPrice;
+    }
 }
--- src/main/java/om/gov/omantax/Main.java
+++ src/main/java/om/gov/omantax/Main.java
@@ -1,7 +1,46 @@
 package om.gov.omantax;
 
+import om.gov.omantax.entity.Customer;
+import om.gov.omantax.entity.Date;
+import om.gov.omantax.entity.Rental;
+import om.gov.omantax.entity.rentalStatus;
+
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Apartment apartment = new Apartment(1, 50.00);
+        Rental rental = new Rental(1, rentalStatus.BOOKED);
+        Date date = new Date("10/10/2023 | 10:30PM","11/10/2023 | 10:30AM");
+        Customer customer = new Customer(1,"Mohammed","97364188");
+
+        try {
+            FileWriter fileWriter = new FileWriter("Rental-Form.txt");
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            bufferedWriter.write("Rental Form     :- "  + "\n"+
+                    "   Apartment ID :" + apartment.getApartmentID() +"\n"+
+                    "Price :" + apartment.getApartmentPrice() +"\n"+
+                    "StartDate :" + date.getStartDateAndTime()+"\n"+
+                    "EndDate :" + date.getEndDateAndTime()+"\n"+
+                    "customer ID :"+ customer.getCustomerId()+"\n"+
+                    "customer Name :"+ customer.getCustomerName()+"\n"+
+                    "customer Mobile :"+ customer.getCustomerMobile()
+
+
+            );
+            bufferedWriter.flush();
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
